65685ab57d22ded1e7ef3aa5da413ea06b67ea6a
anan <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-04-06 19:37:37 +0300

    refactor(i18n): revert locale setup to english only

--- src/shared/i18n/getHomeContent.ts
+++ src/shared/i18n/getHomeContent.ts
@@ -1,12 +1,9 @@
 import { defaultLocale, type Locale } from "./config";
 import type { HomeContent } from "@/shared/content/home.types";
 import { homeContent as enHomeContent } from "@/shared/content/en/home";
-import { homeContent as jaHomeContent } from "@/shared/content/ja/home";
 
 export const getHomeContent = (locale: Locale = defaultLocale): HomeContent => {
   switch (locale) {
-    case "ja":
-      return jaHomeContent;
     case "en":
     default:
       return enHomeContent;
